f0e40a5be44953876459a1c66ba7d1f0ce4e3506
admin <segun3@yandex.ru>
2017-08-01 15:31:09 +0300

    Change the HTML for site.

--- lp/lp/urls.py
+++ lp/lp/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+    url(r'', include('blog.urls')),
 ] \
 		+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
 		+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
